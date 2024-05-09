2044ee0150148113fd18e819e16d84173ac9fc49
l.maldonado.ferrando <lmalfer087@g.educaand.es>
2024-05-09 19:02:42 +0200

    Arreglo SpringSecurity

--- src/main/java/es/laura/saborYNoche/SecurityConfiguration.java
+++ src/main/java/es/laura/saborYNoche/SecurityConfiguration.java
@@ -1,32 +1,32 @@
-package es.laura.saborYNoche;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfiguration {
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/css/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults())
-                .formLogin(form -> form
-                        .loginPage("/loggin")
-                        .loginProcessingUrl("/logginprocess")
-                        .permitAll()
-                );
-
-        return http.build();
-    }
-
-}
+//package es.laura.saborYNoche;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfiguration {
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        http
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .requestMatchers("/css/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .httpBasic(withDefaults())
+//                .formLogin(form -> form
+//                        .loginPage("/login")
+//                        .loginProcessingUrl("/preprocessing")
+//                        .permitAll()
+//                );
+//
+//        return http.build();
+//    }
+//
+//}
